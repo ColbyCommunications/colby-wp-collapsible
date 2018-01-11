44224ed0419ea1fb7ca61bbdd3a69ab99164c9d1
johnwatkins0 <johnwatkins0@gmail.com>
2018-01-11 13:43:01 -0500

    Rebuild

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,3 @@
-import { collapsiblize } from './collapsiblize';
+import Collapsibles from './Collapsibles';
 
-class Collapsibles {
-  static init() {
-    if (!Collapsibles.hasStarted) {
-      Collapsibles.run();
-    }
-  }
-
-  static run() {
-    Collapsibles.hasStarted = true;
-    [...document.querySelectorAll('[data-collapsible]')].forEach(container => {
-      const heading = container.querySelector('.collapsible-heading');
-      const panel = container.querySelector('.collapsible-panel');
-
-      if (heading && panel) {
-        collapsiblize({ heading, panel });
-      }
-    });
-  }
-}
-
-export default Collapsibles;
+window.addEventListener('load', Collapsibles.init);
